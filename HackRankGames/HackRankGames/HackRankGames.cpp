a475b27c3a497d4db62f4f40edf836e30aa2a8d2
jiyongnan <jiyongnan@hotmail.com>
2016-11-24 14:49:55 +0800

    FirstCommit

---
+++ HackRankGames/HackRankGames/HackRankGames.cpp
@@ -0,0 +1,35 @@
+// HackRankGames.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	char s[1000];
+	scanf_s("%s", &s);  //输入字符串。
+	printf("%s\n", s);  //输出字符串。
+
+	//int i = 4;
+	//double d = 4.0;
+	//char s[] = "HackerRank ";
+
+	//// Declare second integer, double, and String variables.
+	//int secondInteger;
+	//double secondDouble;
+	//char secondString[100];
+	//// Read and save an integer, double, and String to your variables.
+	//scanf_s("%d", &secondInteger);
+	//scanf_s("%lf", &secondDouble);
+	//scanf_s("%s", secondString);
+	//// Print the sum of both integer variables on a new line.
+	//printf("%d\n", i + secondInteger);
+	//// Print the sum of the double variables on a new line.
+	//printf("%lf\n", d + secondDouble);
+	//// Concatenate and print the String variables on a new line
+	//printf("%s", s);
+	//printf("%s", secondString);
+	//// The 's' variable above should be printed first.
+	return 0;
+}
+
